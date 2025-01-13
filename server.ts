b40eca1795741315d567feac45f59fc54c8a3504
Vomo319 <omwangheakugbe@gmail.com>
2025-01-13 15:25:57 +0200

    Create server.ts

---
+++ server.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('ChatPWA Backend is running');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('join_chat', (chatId) => {
+    socket.join(chatId);
+    console.log(`User joined chat: ${chatId}`);
+  });
+
+  socket.on('send_message', (message) => {
+    io.to(message.chatId).emit('new_message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
